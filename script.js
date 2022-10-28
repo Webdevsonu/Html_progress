10d4064972ec9de4e2a273ce81a2e324e4bcaa22
Webdevsonu <108911910+Webdevsonu@users.noreply.github.com>
2022-10-28 20:09:40 +0530

    Add files via upload
    This is my first calculator with just HTML, CSS and Basic JavaScript

---
+++ script.js
@@ -0,0 +1,32 @@
+function myFunction7() {
+  document.getElementById("result").value += 7;
+}
+
+function myFunction1() {
+  document.getElementById("result").value += 1;
+}
+
+function myFunction8() {
+  document.getElementById("result").value += 8;
+}
+function myFunction9() {
+  document.getElementById("result").value += 9;
+}
+function myFunction4() {
+  document.getElementById("result").value += 4;
+}
+function myFunction5() {
+  document.getElementById("result").value += 5;
+}
+function myFunction6() {
+  document.getElementById("result").value += 6;
+}
+function myFunction2() {
+  document.getElementById("result").value += 2;
+}
+function myFunction3() {
+  document.getElementById("result").value += 3;
+}
+function myFunction0() {
+  document.getElementById("result").value += 0;
+}
